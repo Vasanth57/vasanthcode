43378c41188f1912c05f49f467d1749b22274359
Vasanth57 <30317765+Vasanth57@users.noreply.github.com>
2017-07-23 09:25:22 +0530

    Update nat.c

--- nat.c
+++ nat.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int n,i,sum=0;
+int number,i,sum=0;
 printf("Enter the number");
-scanf("%d",&n);
-for(i=1;i<=n;++i)
+scanf("%d",&number);
+for(i=1;i<=n;i++)
 {
 sum= sum+i;
 }
